1e24edeb5a57503f168bc7e7f23f2a4ac2055bda
Xezner <prencylabueg@gmail.com>
2022-02-22 22:11:38 +0800

    added collider for ground and hole

--- Hole-io/Assets/OnChangePosition.cs
+++ Hole-io/Assets/OnChangePosition.cs
@@ -15,4 +15,42 @@ public class OnChangePosition : MonoBehaviour
     {
         
     }
+
+    public PolygonCollider2D hole2dCollider;
+    public PolygonCollider2D ground2DCollider;
+    public MeshCollider GeneratedMeshCollider;
+    public float initialScale = 0.5f;
+    Mesh GenMesh;
+    private void FixedUpdate()
+    {
+        if(transform.hasChanged == true)
+        {
+            transform.hasChanged = false;
+            hole2dCollider.transform.position = new Vector2(transform.position.x, transform.position.z);
+            hole2dCollider.transform.localScale = transform.localScale * initialScale;
+            MakeHole2D();
+            MakeMeshCollider();
+        }
+    }
+
+    private void MakeHole2D()
+    {
+        Vector2[] PointPositions = hole2dCollider.GetPath(0);
+
+        for ( int i = 0; i < PointPositions.Length; i++)
+        {
+            PointPositions[i] = hole2dCollider.transform.TransformPoint(PointPositions[i]);
+        }
+
+        ground2DCollider.pathCount = 2;
+        ground2DCollider.SetPath(1, PointPositions);
+    }
+
+
+    private void MakeMeshCollider()
+    {
+        if (GenMesh != null) Destroy(GenMesh);
+        GenMesh = ground2DCollider.CreateMesh(true, true);
+        GeneratedMeshCollider.sharedMesh = GenMesh;
+    }
 }
