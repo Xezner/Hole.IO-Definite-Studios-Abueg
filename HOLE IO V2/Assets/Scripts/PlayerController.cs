191d631dbdf161cf97eaf90d80fbd1050f2e9e5a
Xezner <prencylabueg@gmail.com>
2022-02-23 19:18:08 +0800

    more player controller codes and player direction indicator

--- HOLE IO V2/Assets/Scripts/PlayerController.cs
+++ HOLE IO V2/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
+    public float movementSpeed = 2f;
+    public float verticalInput;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    public void PlayerMovement()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            transform.position += (direction * movementSpeed * Time.deltaTime);
+            //controller.Move(direction * movementSpeed * Time.deltaTime);
+        }
     }
 }
