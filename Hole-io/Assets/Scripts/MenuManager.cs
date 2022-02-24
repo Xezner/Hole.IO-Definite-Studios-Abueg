7b6e12cf0f7144b710b4bd80b10a83bfe1574631
Xezner <prencylabueg@gmail.com>
2022-02-25 00:17:14 +0800

    added leaderboard information on the UI

--- Hole-io/Assets/Scripts/MenuManager.cs
+++ Hole-io/Assets/Scripts/MenuManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
@@ -7,20 +9,26 @@ public class MenuManager : MonoBehaviour
     
     [SerializeField] GameObject fcp = null;
     public bool isSkinsClicked;
+    public TextMeshProUGUI leaderboardsText;
+    public List<string> listStr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        setTopFiveScores();
         fcp.gameObject.SetActive(false);
         isSkinsClicked = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void setTopFiveScores()
     {
+        listStr = PlayerPrefsExtra.GetList<string>("highScoreName");
+        var myComparer = new CustomComparer();
+        listStr.Sort(myComparer);
+        listStr.Reverse();
+        if (listStr.Count > 5) listStr.RemoveRange(5, (listStr.Count - 5));
+        leaderboardsText.text = "Leaderboards\n" + "1. " + listStr[0] + "\n2. " + listStr[1] + "\n3. " + listStr[2] + "\n4. " + listStr[3] +"\n5. " + listStr[4];
     }
 
-    
 
     public void SkinsButtonOnClick()
     {
