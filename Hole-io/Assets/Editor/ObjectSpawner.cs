8ddd67e7cf2b55f2d214e77a43847ed035d5544e
Xezner <prencylabueg@gmail.com>
2022-02-26 19:46:02 +0800

    found a bug in ground scale, committing backup

--- Hole-io/Assets/Editor/ObjectSpawner.cs
+++ Hole-io/Assets/Editor/ObjectSpawner.cs
@@ -49,38 +49,51 @@ public class ObjectSpawner : EditorWindow
         {
             objectName = objectToSpawn.name;
         }
-            float posX = GameObject.Find("Ground").transform.localScale.x/2f;
+            float posX = GameObject.Find("Ground").transform.localScale.x/2f - objectToSpawn.transform.localScale.x/2f;
             minPosX = -posX;
             maxPosX = posX;
-            float posZ = GameObject.Find("Ground").transform.localScale.z / 2f;
+            float posZ = GameObject.Find("Ground").transform.localScale.z / 2f - objectToSpawn.transform.localScale.x / 2f;
             minPosZ = -posZ;
             maxPosZ = posZ;
-
-            Vector3 spawnPos = new Vector3(UnityEngine.Random.Range(minPosX, maxPosX), (objectToSpawn.transform.localScale.y/2f + 0.1f), UnityEngine.Random.Range(minPosZ, maxPosZ));
-
             
-            Collider[] hitColliders = Physics.OverlapBox(spawnPos, objectToSpawn.transform.localScale / 2, Quaternion.identity);
-        int i = 0;
-        if (i < hitColliders.Length)
-        {
-            Debug.LogError("Spawn Point Occupied by: " + hitColliders[i].name);
-            if (hitColliders[i].name == "Player" || hitColliders[i].name == "Ground")
-            {
-                GameObject newObject = Instantiate(objectToSpawn, spawnPos, Quaternion.identity, GameObject.Find("Obstacles").transform);
-                newObject.name = objectName + objectID;
-                objectID++;
-            }
-            else
-            {
-                Debug.Log("DESTROY");
-                //DestroyImmediate(newObject);
-            }
+            Vector3 halfExtents = (objectToSpawn.transform.localScale / 2f);
+            Vector3 spawnPos = new Vector3(UnityEngine.Random.Range(minPosX, maxPosX), 0.4f  , UnityEngine.Random.Range(minPosZ, maxPosZ));            
+            Quaternion yRotate =  Quaternion.AngleAxis(UnityEngine.Random.Range(0, 180), Vector3.up);
 
+        /*Collider[] hitColliders = Physics.OverlapBox(spawnPos, ((objectToSpawn.transform.localScale)*1.1f), yRotate);
+    Debug.Log(yRotate);
+    int i = 0;
+    if (hitColliders.Length > i)
+    {
+        Debug.LogError("Spawn Point Occupied by: " + hitColliders[i].name);
+        if (hitColliders[i].name == "Player" || hitColliders[i].name == "Ground")
+        {
+            GameObject newObject = Instantiate(objectToSpawn, spawnPos, yRotate, GameObject.Find("Obstacles").transform);
+            Debug.Log(yRotate);
+            newObject.name = objectName + objectID;
+            objectID++;
         }
         else
         {
-            GameObject newObject = Instantiate(objectToSpawn, spawnPos, Quaternion.identity, GameObject.Find("Obstacles").transform);
-            newObject.name = objectName + objectID;
+            
+            //DestroyImmediate(newObject);
+        }
+
+    }*/
+        Debug.Log("Half: " + halfExtents);
+        Debug.Log("Spawn.Y: " + spawnPos.y);
+        Debug.Log("Spawn point: " + spawnPos);
+        if (Physics.CheckBox(spawnPos, halfExtents , yRotate))
+        {
+            Debug.Log((Physics.OverlapBox(spawnPos, halfExtents, yRotate))[0]);
+            GameObject newObject = Instantiate(objectToSpawn, spawnPos, yRotate, GameObject.Find("Obstacles").transform);
+            Debug.Log("DESTROY");
+        }
+        else
+        {
+            //GameObject newObject = Instantiate(objectToSpawn, spawnPos, yRotate, GameObject.Find("Obstacles").transform);
+            Debug.Log("SUCCESS");
+            //newObject.name = objectName + objectID;
             objectID++;
         }  
 
