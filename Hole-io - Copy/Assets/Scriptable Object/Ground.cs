1ca17cd73c679df6b311c95a1cc849cb7690b66f
Xezner <prencylabueg@gmail.com>
2022-02-26 11:52:48 +0800

    deleted unused files

--- Hole-io - Copy/Assets/Scriptable Object/Ground.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Ground")]
-public class Ground : ScriptableObject
-{
-    public float groundScale;
-}
